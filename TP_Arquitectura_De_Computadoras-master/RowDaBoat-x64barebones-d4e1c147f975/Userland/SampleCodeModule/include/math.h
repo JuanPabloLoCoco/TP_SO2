c5b41642174ec8f52166ba8f8bba93e46a36d2b4
JuanPabloLoCoco <jlococo@itba.edu.ar>
2018-05-16 20:42:07 -0300

    Subo math.h y .c

---
+++ TP_Arquitectura_De_Computadoras-master/RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/include/math.h
@@ -0,0 +1,17 @@
+#ifndef MATH_H
+#define MATH_H
+#define M_2_PI 6.28318530718f
+#define M_PI_2 1.57079632679f
+#define M_PI 3.14159265359f
+
+void cos(float x, float * res);
+
+void sin(float x, float * res);
+
+void srand(unsigned int seed);
+
+int randM(void); // RAND_MAX assumed to be 32767
+
+int randBound(int min, int max);
+
+#endif
